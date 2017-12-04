65ef1a65ec69e6f7336af5652b2f681f76e1553e
kamalkks <kkamalkks@gmail.com>
2017-12-03 21:52:36 -0800

    Update index.js

--- mmh_hello/index.js
+++ mmh_hello/index.js
@@ -1,1 +1,24 @@
+const hapi = require("hapi");
 
+const server = new hapi.Server();
+server.connection({
+	host: "localhost",
+	port: Number(process.argv[2] || 8000)
+});
+server.route(
+{
+   method:"GET",
+   path:"/",
+   handler:function(request,reply)
+   {
+reply("Hello hapi");
+}
+
+});
+server.start(function(err)
+{
+	if(err)
+	{
+		console.log(err);
+	}
+});
